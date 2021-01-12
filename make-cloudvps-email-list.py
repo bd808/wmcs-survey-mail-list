8312a9a57668319dd16f179921f5edf7b7330aff
Bryan Davis <bd808@wikimedia.org>
2021-01-12 11:31:28 -0700

    make-cloudvps-email-list: relicense as CC0
    This code started from something I wrote that is licensed under the
    GPLv3+ license. I am releasing this derivative under CC0. There is
    nothing here worthy of copyleft virality or attribution claims.

--- make-cloudvps-email-list.py
+++ make-cloudvps-email-list.py
@@ -9,18 +9,7 @@
 # that has never been attached to Wikitech at all (via Striker).
 #
 # Copyright (c) 2021 Bryan Davis and contributors
-#
-# This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option)     # any later version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-# more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program.  If not, see <http://www.gnu.org/licenses/>.
+# CC0 1.0 <https://creativecommons.org/publicdomain/zero/1.0/>
 import collections
 import ldap
 import subprocess
